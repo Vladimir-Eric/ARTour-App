c98745c63e4d37cda75e4e488dae3d41f7e02a4c
Sergej Kaldesic <sergej.kaldesic@gmail.com>
2024-01-11 14:30:49 +0100

    Vremenska prognoza na početnoj stranici

--- app/src/main/java/com/example/artour/PocetnaFragment.java
+++ app/src/main/java/com/example/artour/PocetnaFragment.java
@@ -3,18 +3,141 @@ package com.example.artour;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import java.util.Random;
 
 public class PocetnaFragment extends Fragment {
 
+    private static final String BASE_URL = "https://api.openweathermap.org/data/2.5/";
+    // https://api.openweathermap.org/data/2.5/weather?lat=57&lon=-2.15&units=metric
+    private static final String API_KEY1 = "51f470aceb657d8b45bda10108a0b3d2";
+    private static final String API_KEY2 = "62239b9f50779b246af00153acec56cb";
+    private static final String[] API_KEYS = {API_KEY1, API_KEY2};
+    private static final String CITY_NAME = "Milići";
+
+    private TextView temperatureTextView;
+    private TextView humidityTextView;
+    private TextView weatherInfoTextView;
+    private TextView windSpeedTextView;
+    private ImageView weatherIconImageView;
+
+
+    public static void main(String[] args) {
+        String randomApiKey = getRandomApiKey();
+        System.out.println("Selected API Key: " + randomApiKey);
+    }
+
+    private static String getRandomApiKey() {
+        Random random = new Random();
+        int index = random.nextInt(API_KEYS.length);
+        return API_KEYS[index];
+    }
+
+    private void displayTemperature(double temperature) {
+        temperatureTextView.setText("Temperature: " + temperature);
+    }
+
+    private void displayHumidity(int humidity) {
+        humidityTextView.setText("Humidity: " + humidity + "%");
+    }
+
+    private void displayWeatherInfo(String weatherInfo) {
+        weatherInfoTextView.setText("Weather: " + weatherInfo);
+    }
+
+    private void displayWindSpeed(double windSpeed) {
+        windSpeedTextView.setText("Wind Speed: " + windSpeed + " m/s");
+    }
+
+    private void displayWeatherIcon(String weatherIcon) {
+        // Konstruirajte pun URL za sliku koristeći base URL za ikone od OpenWeather API-ja
+        String iconUrl = "https://openweathermap.org/img/wn/" + weatherIcon + ".png";
+
+        // Učitajte sliku koristeći neku biblioteku poput Picasso ili Glide
+        // Uključite odgovarajuću ovisnost u vašem build.gradle datoteci za Picasso ili Glide
+
+        // Primjer korištenja Picasso biblioteke:
+        Picasso.get().load(iconUrl).into(weatherIconImageView);
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_pocetna, container, false);
+        temperatureTextView = view.findViewById(R.id.temperatureTextView);
+        humidityTextView = view.findViewById(R.id.humidityTextView);
+        weatherInfoTextView = view.findViewById(R.id.weatherInfoTextView);
+        windSpeedTextView = view.findViewById(R.id.windSpeedTextView);
+        weatherIconImageView = view.findViewById(R.id.weatherIconImageView);
+
+        // Inicijalizirajte Retrofit instancu
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        // Nastavite s korištenjem Retrofit instance za dohvaćanje podataka
+        // ...
+
+        fetchWeatherData();
 
         return view;
+    }
+
+    private void fetchWeatherData() {
+        // Inicijalizirajte Retrofit objekt
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL) //http://openweathermap.org/data/2.5
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        // Inicijalizirajte WeatherService pomoću Retrofit objekta
+        WeatherService weatherService = retrofit.create(WeatherService.class);
+
+        // Napravite poziv prema API-ju
+        Call<WeatherResponse> call = weatherService.getWeather(CITY_NAME, getRandomApiKey());
+
+        // Izvršite asinkroni poziv
+        call.enqueue(new Callback<WeatherResponse>() {
+            public void onResponse(Call<WeatherResponse> call, Response<WeatherResponse> response) {
+                if (response.isSuccessful()) {
+                    WeatherResponse weatherResponse = response.body();
+                    if (weatherResponse != null) {
+                        double temperature = weatherResponse.getMainInfo().getTemperature();
+                        int humidity = weatherResponse.getMainInfo().getHumidity();
+                        String weatherInfo = weatherResponse.getWeatherInfo()[0].getWeatherMain();
+                        double windSpeed = weatherResponse.getWindInfo().getWindSpeed();
+                        String weatherIcon = weatherResponse.getWeatherInfo()[0].getWeatherIcon();
+
+                        displayTemperature(temperature);
+                        displayHumidity(humidity);
+                        displayWeatherInfo(weatherInfo);
+                        displayWindSpeed(windSpeed);
+                        displayWeatherIcon(weatherIcon);
+                    }
+                } else {
+                    // Obrada neuspješnog odgovora
+                }
+            }
+
+            @Override
+            public void onFailure(Call<WeatherResponse> call, Throwable t) {
+                // Handle failure
+            }
+        });
     }
 }
