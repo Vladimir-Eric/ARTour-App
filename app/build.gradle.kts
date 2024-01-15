b0f5a3f290153eb88b8df5ed8db6c0128cfdaa0c
Sergej Kaldesic <sergej.kaldesic@gmail.com>
2024-01-15 23:02:39 +0100

    Kartica za destinacije dizajn

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,6 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment:2.7.6")
     implementation("androidx.navigation:navigation-ui:2.7.6")
-    implementation("androidx.preference:preference:1.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -55,5 +54,4 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.picasso:picasso:2.71828")
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
 }
